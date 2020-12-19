570099fecd872ed7dc6ce5ba55e62d41551e76b7
carlosrobinson <carlos.robinson@bryanuniversity.edu>
2020-12-19 17:14:44 -0500

    first commit

---
+++ week1/todo-app/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <input type= "checkbox" />
+            <p>Change oil</p>
+            <input type= "checkbox" />
+            <p>Cut Grass</p>
+            <input type= "checkbox" />
+            <p>Feed the Dog</p>
+            <input type= "checkbox" />
+            <p>Do Laundry</p>
+        </div>
+    )
+}
+export default App
